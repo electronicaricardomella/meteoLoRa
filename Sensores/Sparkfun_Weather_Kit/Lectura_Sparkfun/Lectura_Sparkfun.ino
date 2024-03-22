913636ed7fc821bc674e1db3610f4c73fe5c8baf
Guille <137082665+Yvky@users.noreply.github.com>
2024-03-22 10:45:40 +0100

    Add files via upload

---
+++ Sensores/Sparkfun_Weather_Kit/Lectura_Sparkfun/Lectura_Sparkfun.ino
@@ -0,0 +1,60 @@
+#include "SparkFun_Weather_Meter_Kit_Arduino_Library.h"
+
+// Below are the pin definitions for each sensor of the weather meter kit
+
+// Pins for Weather Carrier with ESP32 Processor Board
+int windDirectionPin = 35;
+int windSpeedPin = 14;
+int rainfallPin = 27;
+
+// Pins for the Weather Shield with SparkFun RedBoard Qwiic or Arduino Uno
+// int windDirectionPin = A0;
+// int windSpeedPin = 3;
+// int rainfallPin = 2;
+
+// Create an instance of the weather meter kit
+SFEWeatherMeterKit weatherMeterKit(windDirectionPin, windSpeedPin, rainfallPin);
+
+void setup()
+{
+    // Begin serial
+    Serial.begin(115200);
+    Serial.println(F("SparkFun Weather Meter Kit Example 1 - Basic Readings"));
+    Serial.println();
+    Serial.println(F("Note - this example demonstrates the minimum code required"));
+    Serial.println(F("for operation, and may not be accurate for your project."));
+    Serial.println(F("It is recommended to check out the calibration examples."));
+
+    // Expected ADC values have been defined for various platforms in the
+    // library, however your platform may not be included. This code will check
+    // if that's the case
+#ifdef SFE_WMK_PLAFTORM_UNKNOWN
+    // The platform you're using hasn't been added to the library, so the
+    // expected ADC values have been calculated assuming a 10k pullup resistor
+    // and a perfectly linear 16-bit ADC. Your ADC likely has a different
+    // resolution, so you'll need to specify it here:
+    weatherMeterKit.setADCResolutionBits(10);
+    
+    Serial.println(F("Unknown platform! Please edit the code with your ADC resolution!"));
+    Serial.println();
+#endif
+
+    // Begin weather meter kit
+    weatherMeterKit.begin();
+}
+
+void loop()
+{
+    // Print data from weather meter kit
+    Serial.print(F("Wind direction (degrees): "));
+    Serial.print(weatherMeterKit.getWindDirection(), 1);
+    Serial.print(F("\t\t"));
+    Serial.print(F("Wind speed (kph): "));
+    Serial.print(weatherMeterKit.getWindSpeed(), 1);
+    Serial.print(F("\t\t"));
+    Serial.print(F("Total rainfall (mm): "));
+    Serial.println(weatherMeterKit.getTotalRainfall(), 1);
+
+    // Only print once per second
+    delay(1000);
+}
